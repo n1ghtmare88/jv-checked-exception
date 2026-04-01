274c99b94dbcf981727bb72784ab465a4b1b2d48
Dmitriy Yevtushenko <dmitriy.zp.city@gmail.com>
2026-04-01 16:15:23 +0300

    ht is done

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -4,9 +4,15 @@ public class PasswordValidator {
     private boolean isPasswordValid;
     private boolean isLengthValid;
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
+    public void validate(String password, String repeatPassword)
+            throws PasswordValidationException {
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
         isPasswordValid = password.equals(repeatPassword);
         isLengthValid = password.length() >= 10 && repeatPassword.length() >= 10;
-        //return (isPasswordValid && isLengthValid) ? true : false;
+        if (!isPasswordValid || !isLengthValid) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -4,11 +4,11 @@ public class UserService {
     private final PasswordValidator passwordValidator = new PasswordValidator();
 
     public void registerUser(User user) {
-        boolean result = false;
         try {
             passwordValidator.validate(user.getPassword(), user.getRepeatPassword());
+            saveUser(user);
         } catch (PasswordValidationException e) {
-            System.out.println("Invalid password");
+            System.out.println("Your passwords are incorrect. Try again.");
         }
     }
 
